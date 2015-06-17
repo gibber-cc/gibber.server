10dae60a197fab676adc590db3b73d14abd2012a
charlieroberts <charlie@charlie-roberts.com>
2015-06-17 15:56:47 -0700

    changed access credentials for sample loading, modified share.js version required

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', ["http://127.0.0.1:8080"]);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
    // res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Credentials', 'true')
+  res.header('Access-Control-Allow-Credentials', 'false')
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept')
   next();
 }
